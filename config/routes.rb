af18fe65e0d5b4ace1591190145a9016f64da13d
kevinrudolphchicago <kevinrudolph571@gmail.com>
2015-07-29 16:46:09 -0700

    Rudolph_photogram_phase_1

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
-
+  get("/delete_photo/:id",       { :controller => "photos", :action => "destroy" })
+    get("/photos/:id/edit",       { :controller => "photos", :action => "edit" })
+  get("/photos/:id",       { :controller => "photos", :action => "new_form" })
 end
